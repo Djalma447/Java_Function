3324677f8ca04e431e07dd4c68886748678e7576
Djalma <djalma.ant@gmail.com>
2021-12-08 14:01:21 -0300

    Interface implementation

---
+++ src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import entities.Product;
+import util.UpperCaseName;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Playstation 5", 5999.99));
+		list.add(new Product("Iphone 11", 4999.90));
+		list.add(new Product("TV Lg", 3999.99));
+		list.add(new Product("The Sims 4", 99.90));
+		list.add(new Product("Mafia III", 48.50));
+		list.add(new Product("Xcom 2", 9.99));
+		
+		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+		
+		names.forEach(System.out::println);
+
+	}
+
+}
